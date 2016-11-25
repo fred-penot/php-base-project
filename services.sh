909a81803c0809bda8bb7baa75b06fd37e997760
fred-penot <frederic.penot@gmail.com>
2016-11-25 22:35:01 +0100

    Add files via upload

---
+++ services.sh
@@ -0,0 +1,3 @@
+service ssh start > /home/${login_ssh}/scripts/services.log
+service zend-server start >> /home/${login_ssh}/scripts/services.log
+service mysql start >> /home/${login_ssh}/scripts/services.log
