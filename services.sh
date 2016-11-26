3876b1bee582d4ab6f9d5bd090b3fae79d198e1a
fred-penot <frederic.penot@gmail.com>
2016-11-26 09:45:55 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,3 +1,27 @@
 service ssh start
 service zend-server start
 service mysql start
+
+echo ''
+echo ''
+echo ''
+echo '###############################################################################'
+echo '##                                                                             '
+echo '##                   Bienvenue sur le container de Base ZS9                    '
+echo '##                                                                             '
+echo '##                                                                             '
+echo '##    * Page d accueil du serveur : http://172.17.0.2                          '
+echo '##                                                                             '
+echo '##    * Connexion SSH :                                                        '
+echo '##      - host => 172.17.0.2                                                   '
+echo '##      - login => ${login_ssh}                                                '
+echo '##      - password => ${password_ssh}                                          '
+echo '##                                                                             '
+echo '##    * Connexion MySQL :                                                      '
+echo '##      - login => root                                                        '
+echo '##      - password => ${password_mysql}                                        '
+echo '##                                                                             '
+echo '###############################################################################'
+echo ''
+echo ''
+echo ''
